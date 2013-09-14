80a647a5cb818d6b4b133317f8d4ba4be9d44b88
Merve Civelek <mrvcivelek@hotmail.com>
2013-09-14 11:18:11 +0300

    Fixed bug with user profile validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -41,4 +41,12 @@ class UserTest < ActiveSupport::TestCase
  	assert !user.errors[:profile_name].empty?
  	assert user.errors[:profile_name].include?("Must be formatted correctly.")
  end
+
+ test "a user can have a correctly formatted profile name" do
+ 	user = User.new(first_name: 'Merve', last_name: 'Civelek', email: 'mrvcivelek@gmail.com')
+ 	user.password = user.password_confirmation = 'foobar89'
+
+ 	user.profile_name = 'mrvcivelek'
+ 	assert user.valid?
+ end
 end
