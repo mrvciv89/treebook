0434667d1723b2a69ea1d0fe1f4433db5ad71dc2
Merve Civelek <mrvcivelek@hotmail.com>
2013-09-15 23:31:55 +0300

    Added frienship to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,16 @@ has_many :statuses
   def full_name
   	first_name + " " + last_name
   end
+
+  def to_param
+    profile_name
+  end
+
+  def gravatar_url
+    stripped_email = email.strip
+    downcased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcased_email)
+
+    "http://gravatar.com/avatar/#{hash}"
+  end
 end
