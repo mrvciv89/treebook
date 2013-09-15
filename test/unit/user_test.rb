fc23a6992c682a3e2997b5dafff7edc759254e17
Merve Civelek <mrvcivelek@hotmail.com>
2013-09-15 15:44:26 +0300

    Add user friendships and associated tests.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,54 +1,69 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
- test "a user should enter a first name" do 
- 	user = User.new
- 	assert !user.save
- 	assert !user.errors[:first_name].empty?
- end
+  should have_many(:statuses)
+  should have_many(:user_friendships)
+  should have_many(:friends)
 
- test "a user should enter a last name" do 
- 	user = User.new
- 	assert !user.save
- 	assert !user.errors[:last_name].empty?
- end
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
 
-  test "a user should enter a profile name" do 
- 	user = User.new
- 	assert !user.save
- 	assert !user.errors[:profile_name].empty?
- end
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
 
- test "a user should have a unique profile name" do 
- 	user = User.new
- 	user.profile_name = 'mrvciv'
- 	user.email= "mrvcivelek@hotmail.com"
- 	user.first_name= "Merve"
- 	user.last_name= "Civelek"
- 	user.password= "05348605914"
- 	user.password_confirmation="05348605914"
+  test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
 
- 	assert !user.save
- 	puts user.errors.inspect
- 	assert !user.errors[:profile_name].empty?
- end
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:jason).profile_name
 
- test "a user should have a profile name without spaces" do 
- 	user = User.new(first_name: 'Merve', last_name: 'Civelek', email: 'mrvcivelek@gmail.com')
- 	user.password = user.password_confirmation = 'foobar89'
- 	
- 	user.profile_name = "My Profile with spaces"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
 
- 	assert !user.save
- 	assert !user.errors[:profile_name].empty?
- 	assert user.errors[:profile_name].include?("Must be formatted correctly.")
- end
+  test "a user should have a profile name without spaces" do
+    user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
+    user.password = user.password_confirmation = 'asdfasdf'
 
- test "a user can have a correctly formatted profile name" do
- 	user = User.new(first_name: 'Merve', last_name: 'Civelek', email: 'mrvcivelek@gmail.com')
- 	user.password = user.password_confirmation = 'foobar89'
+    user.profile_name = 'Contains spaces'
 
- 	user.profile_name = 'mrvcivelek'
- 	assert user.valid?
- end
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user should have a profile name" do
+    user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.profile_name = 'jasonseifer_1'
+    assert user.valid?
+  end
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:jason).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do
+    users(:jason).friends << users(:mike)
+    users(:jason).friends.reload
+    assert users(:jason).friends.include?(users(:mike))
+  end
+
+  test "that creating friendships based on a user id and friend id works" do
+    UserFriendship.create user_id: users(:jason).id, friend_id: users(:mike).id
+    assert users(:jason).friends.include?(users(:mike))
+  end
 end
